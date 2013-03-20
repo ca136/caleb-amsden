3c1c29b21089b3ef298889bdbcf22410f8803a98
Caleb <caleb.amsden@gmail.com>
2013-03-20 11:07:48 -0700

    Initial commit for personal site

---
+++ app.py
@@ -0,0 +1,168 @@
+from flask import Flask, url_for, redirect, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext import admin, login, wtf
+from flask.ext.admin.contrib import sqlamodel
+from flask.ext.admin.contrib.fileadmin import FileAdmin
+
+import os.path as op
+
+# Create application
+app = Flask(__name__)
+
+# Create dummy secrey key so we can use sessions
+app.config['SECRET_KEY'] = '123456790'
+
+# Create in-memory database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+# Create user model. For simplicity, it will store passwords in plain text.
+# Obviously that's not right thing to do in real world application.
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(64))
+
+    # Flask-Login integration
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    # Required for administrative interface
+    def __unicode__(self):
+        return self.username
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    headline = db.Column(db.String(64), unique=True)
+    body = db.Column(db.Text)
+#    publish_date = db.relationship('Size', backref='product')
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    headline = db.Column(db.String(64), unique=True)
+    body = db.Column(db.Text)
+#    publish_date = db.relationship('Size', backref='product')
+
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255), unique=True)
+    main_photo = db.Column(db.Boolean)
+
+
+# Define login and registration forms (for flask-login)
+class LoginForm(wtf.Form):
+    login = wtf.TextField(validators=[wtf.required()])
+    password = wtf.PasswordField(validators=[wtf.required()])
+
+    def validate_login(self, field):
+        user = self.get_user()
+
+        if user is None:
+            raise wtf.ValidationError('Invalid user')
+
+        if user.password != self.password.data:
+            raise wtf.ValidationError('Invalid password')
+
+    def get_user(self):
+        return db.session.query(User).filter_by(login=self.login.data).first()
+
+
+# Initialize flask-login
+def init_login():
+    login_manager = login.LoginManager()
+    login_manager.setup_app(app)
+
+    # Create user loader function
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.query(User).get(user_id)
+
+# Create customized model view class
+class MyModelView(sqlamodel.ModelView):
+    def is_accessible(self):
+        return login.current_user.is_authenticated()
+
+
+# Create customized index view class
+class MyAdminIndexView(admin.AdminIndexView):
+    def is_accessible(self):
+        return login.current_user.is_authenticated()
+
+# Render custom index for admin
+class MyView(admin.BaseView):
+    @admin.expose('/')
+    def index(self):
+        return self.render('index.html')
+
+@app.route('/')
+def index():
+    notes = Note.query.all()
+    return render_template('index.html', user=login.current_user, notes=notes)
+
+@app.route('/login/', methods=('GET', 'POST'))
+def login_view():
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user = form.get_user()
+        login.login_user(user)
+        return redirect(url_for('index'))
+
+    return render_template('form.html', form=form)
+
+@app.route('/register/', methods=('GET', 'POST'))
+def register_view():
+    form = RegistrationForm(request.form)
+    if form.validate_on_submit():
+        user = User()
+
+        form.populate_obj(user)
+
+        db.session.add(user)
+        db.session.commit()
+
+        login.login_user(user)
+        return redirect(url_for('index'))
+
+    return render_template('form.html', form=form)
+
+
+@app.route('/logout/')
+def logout_view():
+    login.logout_user()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    # Initialize flask-login
+    init_login()
+
+    # Create admin
+    admin = admin.Admin(app, 'Website', index_view=MyAdminIndexView())
+    path = op.join(op.dirname(__file__), 'files')
+
+    # Add views
+    admin.add_view(FileAdmin(path, '/files/', name='Files'))
+    admin.add_view(sqlamodel.ModelView(User, db.session))
+    admin.add_view(sqlamodel.ModelView(Note, db.session))
+    #admin.add_view(ProductAdmin(Product, db.session))
+
+    # Create DB
+    db.create_all()
+
+    # Start app
+    app.debug = True
+    app.run()
